c936b33624f4bd4c08dc3bd12c4dbcd00539c34c
eisa <g45@outlook.com>
2022-05-20 21:50:12 +0200

    SignUp Database
    -fixed con string to variable
    -fixed INSERT string

--- new Moives/Signup.aspx.cs
+++ new Moives/Signup.aspx.cs
@@ -19,14 +19,17 @@ namespace new_Moives
 
         protected void Signup_Click(object sender, EventArgs e)
         {
-        SqlConnection w = new SqlConnection();
- 
-        w.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\abdel\\source\\repos\\new Moives\\App_Data\\member.mdf;Integrated Security=True";
-        String z = String.Format("insert into Member values('{0}','{1}','{2}','{3}','{4}',)  ", username.Text, password.Text, firstname.Text, lastname.Text, emailaddress.Text);
-        SqlCommand cmd = new SqlCommand(z, w);
-        w.Open();
-        cmd.ExecuteNonQuery();
-        w.Close();
+            SqlConnection con = new SqlConnection();
+
+            //jesus : Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename="E:\some app\movie-rentes\new Moives\App_Data\member.mdf";Integrated Security=True
+            //mkky : w.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\abdel\\source\\repos\\new Moives\\App_Data\\member.mdf;Integrated Security=True";
+
+            con.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|member.mdf;Integrated Security=True";
+            String z = String.Format("INSERT INTO member VALUES('{0}','{1}','{2}','{3}')", username.Text,emailaddress.Text, password.Text, RadioButtonList1.SelectedValue);
+            SqlCommand cmd = new SqlCommand(z, con);
+            con.Open();
+            cmd.ExecuteNonQuery();
+            con.Close();
         }
 
         protected void password_TextChanged(object sender, EventArgs e)
